4d30deee5b4d53fc99b07f455d01afae9ef0c1c8
Dolph <alnorob@outlook.com>
2022-05-22 01:22:56 +0100

    ported morbius sword from test mod i made

---
+++ Items/MorbiusSword.cs
@@ -0,0 +1,38 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WBFunnyMod.Items
+{
+	public class MorbiusSword : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("morbius movie");
+			Tooltip.SetDefault("swing the morbius sword call that a morbius sweep");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 14;
+			item.melee = true;
+			item.width = 1600;
+			item.height = 1600;
+			item.useTime = 15;
+			item.useAnimation = 15;
+			item.useStyle = 1;
+			item.knockBack = 10;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
