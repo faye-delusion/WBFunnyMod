29b2f18ecf69602c5c9f8d432cf6f6607a512244
Dolph <alnorob@outlook.com>
2022-05-22 01:26:14 +0100

    add constructor to WBFunnyMod.cs

--- WBFunnyMod.cs
+++ WBFunnyMod.cs
@@ -4,5 +4,21 @@ namespace WBFunnyMod
 {
 	public class WBFunnyMod : Mod
 	{
+
+		public WBFunnyMod()
+		{
+
+			Properties = new ModProperties()
+			{
+
+				Autoload = true,
+				AutoloadGores = true,
+				AutoloadSounds = true
+
+			};
+
+		}
+
 	}
+
 }
